e9759d1ba4a861f6bc60e21a7a9a700f155972cd
josebroldant@gmail.com <jrolda3@bancodebogota.com.co>
2019-10-10 11:54:59 -0500

    funciona, pero falta lo del http

--- application/src/app/app.module.ts
+++ application/src/app/app.module.ts
@@ -1,24 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
 import { InicioComponent } from './components/inicio/inicio.component';
+import { RouterModule } from '@angular/router';
 import { NotfoundComponent } from './components/notfound/notfound.component';
+import { LoginComponent } from './components/login/login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { ClienteService } from './services/cliente.service';
+import { Servicio1Service } from './services/servicio1.service';
+import { ActivatedRoute } from '@angular/router';
+import { UserNameService } from 'src/app/services/userNameService.service';
+
+
+//ALL THIS CODE IS JUST FOR INCLUDING COMPONENTS OF ANGULAR AND IMPORT AND EXPORT ELEMENTS BETWEEN COMPONENTS
+
+
 
 @NgModule({
+  
   declarations: [
     AppComponent,
-    LoginComponent,
     InicioComponent,
-    NotfoundComponent
+    NotfoundComponent,
+    LoginComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule,
+    HttpClientModule
+    
   ],
-  providers: [],
+  providers: [Servicio1Service, ClienteService, UserNameService],//ALWAYS CALL SERVICES HERE IN MODULE.TS
   bootstrap: [AppComponent]
 })
 export class AppModule { }
