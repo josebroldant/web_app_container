7ca7ca779bf159298792281cf3724a27884943de
unknown <josebroldant@gmail.com>
2020-04-16 19:23:03 -0500

    resolved cors

--- application/src/server.js
+++ application/src/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express(); 
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var json_vacio = {"voltage":"", "current":"", "power":"", "level":"", "state":""};
 /*
 const db = require("./db");
 const dbName = "variables";
@@ -14,6 +15,19 @@ mongoose.Promise = global.Promise;
 app.listen(8081, function(){
     console.log("Listening to port 8081");
 });
+
+var angular = 'http://localhost:4200';
+//PERMISOS DE ACCESO PARA LA APLICACION
+app.use(function (req, res, next){
+  res.setHeader('Access-Control-Allow-Origin', angular);
+  res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, OPTIONS, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
+
+
 
 //CONEXION A MONGODB ATLAS
 
@@ -49,7 +63,11 @@ app.post('/', function(req, res) {
       "state": req.body["state"],
       "level": req.body["level"]
     }
-    
+
+    //PARA EL GET DE ANGULAR
+    json_vacio = json_final;
+    ////////////////////////
+
     console.log(json_final);
 
     var newDatos = new modelo(json_final);//wrap data into a mongodb model 
@@ -61,20 +79,7 @@ app.post('/', function(req, res) {
       .catch(err => {
         res.status(400).send("cannot save");
         console.log("cannot be saved");
-      });
-      
-
-    /* //USING ANOTHER METHOD
-    const item = req.body;
-      dbCollection.insertOne(item, (error, result) => { // callback of insertOne
-        if (error) throw error;
-        // return updated list
-        dbCollection.find().toArray((_error, _result) => { // callback of find
-            if (_error) throw _error;
-            response.json(_result);
-        });
-    });
-    */  
+      });  
       
 });
 
@@ -100,10 +105,10 @@ var modelo = mongoose.model("modelo", Schema);
 //module.exports = modelo;
 
 
-//post data to mongodb
+//send data to angular app
 
-app.post('/test', function (req, res) {
-  console.log(req.body);
+app.get('/test', function (req, res) {
+  res.send(json_vacio);
 });
 
 app.post('/send', async (req, res) => {
