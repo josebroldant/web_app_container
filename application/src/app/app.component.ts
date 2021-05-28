cc2f42759f278cc42cd3ef3cfee7ffa1f6d8d387
josebroldant <josebroldant@gmail.com>
2021-05-28 15:19:45 -0500

    app component?

--- application/src/app/app.component.ts
+++ application/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, Directive, Pipe, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { ClienteService } from './services/cliente.service';
-import { LoginService } from './services/login.service';
 import { FormGroup, FormControl, Validators, FormsModule } from '@angular/forms';
 import { HttpClient, HttpParams, HttpHeaders, HttpRequest} from '@angular/common/http';
+import { ClienteService } from './services/cliente.service';
+import { LoginService } from './services/login.service';
 import { MongoService } from './services/mongo.service';
+import { UserNameService } from './services/userNameService.service';
 
 
 //ALL THIS CODE IS ABOUT CLASSES AND FUCNTIONS AND LOGICAL ACTIONS OF THE PROGRAM
@@ -13,52 +14,22 @@ import { MongoService } from './services/mongo.service';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ClienteService, MongoService]
+  providers: [Router, ClienteService, MongoService, LoginService]
   
 })
 export class AppComponent {
-
-  fontSize: string;
-  color: string;
     
-    usuario = '';
-    contra = '';
-    loggedin = false;
-    
-    constructor(private router: Router , private cliente: ClienteService, private metodo: LoginService, private newService: MongoService){
+    constructor(private router: Router , private clienteserv: ClienteService, private loginserv: LoginService, private mongoserv: MongoService){
       /*
       mensaje.getEstado().subscribe(estado => console.log(estado));//DESPUES DE HACER LA FUNCION, UTILIZA SUBSCRIBE,
       //DESDE EL METODO OF DECLARADO EN EL SERVICIO PARA POSTEAR EL ESTADO EN CONSOLA EN ESTE CASO
       mensaje.log('INFO','creating app component');//LA VARIABLE MENSAJE RECIBE LOS PARAMETROS DE LA FUNCION LOG DECLARADA EN EL SERVICIO
       mensaje.getEstado().subscribe(estado => console.log(estado));
       */
+
     }
 
     //LOCAL METHODS
-    onSave = function(user, isValid: boolean){
-      user.mode = this.ValButton;
-      this.newService.saveUser(user).subscribe(data => {alert(data.data);
-      this.ngOnInit();
-    }
-    , error => this.errorMessage = error)
-
-    }
-
-    edit = function(kk){
-      this.id = kk._id;
-      this.voltaje = kk.voltaje;
-      this.corriente = kk.corriente;
-      this.potencia = kk.potencia;
-      this.llenado = kk.llenado;
-      this.estado = kk.estado;
-    }
-
-    delete = function(id){
-      this.newService.deleteUser(id).subscribe(data => {alert(data.data);
-      this.ngOnInit();}, error => this.errorMessage = error)
-    }
-
-    
     navigateTo(to: string){
       this.router.navigate([to]);/*this.router.navigate([to, this.usuario])    --->   /terms/usuario   */ 
     }    
